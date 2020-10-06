4e5dad5168607a2241271579595efe0c139ecb2c
rmmartinez929294@gmail.com <59756487+IlidanStormrage@users.noreply.github.com>
2020-10-06 02:00:18 -0500

    Server.js TERMINADO

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT= process.env.PORT | 5000;
+app.get('/', (req, res) => res.send('API RUNNING'));
+const PORT = process.env.PORT || 5000;
 
-app.listen();
- 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
